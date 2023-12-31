0c7aff681793835779148f4625836a869c4dd9c1
Mike Sanocki <sanockimike@gmail.com>
2023-12-30 19:03:25 -0500

    v7

--- server/functions/api.js
+++ server/functions/api.js
@@ -8,22 +8,22 @@ import serverless from "serverless-http";
 import "../src/db/mongoose.js";
 
 // Routes
-import userRouter from "../src/routes/user.js";
-import reviewRouter from "../src/routes/review.js";
-import authRouter from "../src/routes/auth.js";
-import movieRouter from "../src/routes/movies.js";
-import watchListRouter from "../src/routes/watchLists.js";
+// import userRouter from "../src/routes/user.js";
+// import reviewRouter from "../src/routes/review.js";
+// import authRouter from "../src/routes/auth.js";
+// import movieRouter from "../src/routes/movies.js";
+// import watchListRouter from "../src/routes/watchLists.js";
 
 const app = express();
 
 app.use(json());
 app.use(cors());
 
-app.use("/", authRouter);
-app.use("/", userRouter);
-app.use("/", reviewRouter);
-app.use("/", movieRouter);
-app.use("/", watchListRouter);
+// app.use("/", reviewRouter);
+// app.use("/", authRouter);
+// app.use("/", userRouter);
+// app.use("/", movieRouter);
+// app.use("/", watchListRouter);
 
 const router = express.Router();
 
@@ -33,5 +33,4 @@ router.get("/de", (req, res) => {
   });
 });
 
-app.use("/", router);
 export const handler = serverless(app);
